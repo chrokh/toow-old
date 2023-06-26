55fd0288983ba28c1e817b65ef06a81596e31dc4
chrokh <chrokh@users.noreply.github.com>
2023-06-26 15:40:48 +0000

    deploy: d4629cee879e12601268a5f5d20787eb69935f99

--- _static/custom.js
+++ _static/custom.js
@@ -14,3 +14,39 @@ document.addEventListener('DOMContentLoaded', function() {
     }
   });
 });
+
+
+// Scroll in TOC navigation so that current title is visible.
+document.addEventListener('DOMContentLoaded', function() {
+  // Find the first h1 element and grab its text
+  var h1Elements = document.getElementsByTagName('h1');
+  var currentTitle = h1Elements[0].innerText;
+
+  // Find the first list item that contains the currentTitle
+  var currentTocItem = findListItemByText(currentTitle);
+
+  // Scroll the sidebar-scroll div to make the currentTocItem appear in the top third
+  if (currentTocItem) {
+    var sidebarScrollDiv = document.querySelector('.sidebar-scroll');
+    var sidebarHeight = sidebarScrollDiv.offsetHeight;
+    var listItemTopOffset = currentTocItem.offsetTop;
+    var topThirdHeight = sidebarHeight / 3;
+    var scrollToY = listItemTopOffset - (2 * topThirdHeight);
+    sidebarScrollDiv.scrollTop = scrollToY;
+  }
+});
+
+function findListItemByText(text) {
+  // Find all the list items on the page
+  var listItems = document.getElementsByTagName('li');
+
+  // Loop through each list item and check if its text contains the given text
+  for (var i = 0; i < listItems.length; i++) {
+    var listItemText = listItems[i].innerText;
+    if (listItemText.includes(text)) {
+      return listItems[i];
+    }
+  }
+
+  return null;
+}
